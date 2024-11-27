f87452aaccdbf1bc5bb79f81f98fdb91fedceb6a
Vitali <vitaly.u@mail.ru>
2024-11-27 15:07:46 +0300

    добавлены слова широта и долгота

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ def get_coordinates(city, key):
         if results:
             lat = round(results[0]['geometry']['lat'], 2)
             lon = round(results[0]['geometry']['lng'], 2)
-            return lat, lon
+            return f"Широта: {lat}, Долгота: {lon}"
         else:
             return "Город не найден"
     except Exception as e:
         return f"Возникла ошибка: {e}"
 
 key = '8e8b65190223494b8c695ba83eb4b1fe'
-city = "Химки"
+city = "Эквадор"
 coordinates = get_coordinates(city, key)
 print(f"Координаты города {city}: {coordinates}")
